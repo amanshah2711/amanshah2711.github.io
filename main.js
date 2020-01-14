c44f9c6d5ac7e4d6045803ebf7667099f7d9d2d7
Aman Shah <amanshah2711@berkeley.edu>
2020-01-14 03:40:31 -0500

    Updating CSS/JS for sp20

---
+++ main.js
@@ -0,0 +1,35 @@
+var curr = "#sp20-content"
+
+function test(input){
+    $(curr).hide();
+    curr = input;
+    $(curr).slideDown();
+}
+$(document).ready(function() {
+    // all custom jQuery will go here
+    $.each([
+        'fa19.html',
+        'csm-fa19.html'
+    ], function(i,a){
+        $("#main").load(a);
+    });;
+    $("#fa19-content").hide();
+    $("#csm-fa19-content").hide();
+    $("#csm-sp20-content").hide();
+    $("#su19-content").hide();
+    $("#sp20").click(function() {
+        test("#sp20-content");
+    })
+    $("#csm-sp20").click(function() {
+        test("#csm-sp20-content")
+    })
+    $("#fa19").click(function(){
+        test("#fa19-content");
+    })
+    $("#csm-fa19").click(function() {
+        test("#csm-fa19-content")
+    })
+    $("#su19").click(function(){
+        test("#su19-content");
+    })
+});
