1e0d2251da807e67452b7bae64d3022a9a8418d5
Aman Shah <amanshah2711@berkeley.edu>
2020-01-14 05:37:27 -0500

    hopefully resolve

--- main.js
+++ main.js
@@ -2,36 +2,30 @@ var curr = "#sp20-content";
 
 function test(input){
     $(curr).hide();
-    console.log(curr)
+    console.log(curr);
     curr = input;
     $(curr).slideDown();
 }
 $(document).ready(function() {
     // all custom jQuery will go here
-    $.each([
-        'fa19.html',
-        'csm-fa19.html',
-        'su19.html',
-        'csm-sp20.html',
-        'sp20.html'
-    ], function(i,a){
-        $("#main").load(a);
-    });
-    $("#fa19-content").hide();
-    $("#csm-fa19-content").hide();
-    $("#csm-sp20-content").hide();
-    $("#su19-content").hide();
+
+    $.get("https://amanshah2711.github.io/fa19.html", '', function (data) { $("#main").append(data); });
+    $.get("https://amanshah2711.github.io/csm-fa19.html", '', function (data) { $("#main").append(data); });
+    $.get("https://amanshah2711.github.io/su19.html", '', function (data) { $("#main").append(data); });
+    $.get("https://amanshah2711.github.io/sp20.html", '', function (data) { $("#main").append(data); });
+    $.get("https://amanshah2711.github.io/csm-sp20.html", '', function (data) { $("#main").append(data); });
+
     $("#sp20").click(function() {
         test("#sp20-content");
     })
     $("#csm-sp20").click(function() {
-        test("#csm-sp20-content")
+        test("#csm-sp20-content");
     })
     $("#fa19").click(function(){
         test("#fa19-content");
     })
     $("#csm-fa19").click(function() {
-        test("#csm-fa19-content")
+        test("#csm-fa19-content");
     })
     $("#su19").click(function(){
         test("#su19-content");
