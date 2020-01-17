784f93715bf19da042823e076c45b4505a8b1ccb
Aman Shah <amanshah2711@berkeley.edu>
2020-01-16 21:25:20 -0500

    Restructing directory

---
+++ scripts/main.js
@@ -0,0 +1,33 @@
+var curr = "#sp20-content";
+
+function test(input){
+    $(curr).hide();
+    console.log(curr);
+    curr = input;
+    $(curr).slideDown();
+}
+$(document).ready(function() {
+    // all custom jQuery will go here
+
+    $.get("https://amanshah2711.github.io/cs61a/discussion/fa19/fa19.html", '', function (data) { $("#main").append(data); });
+    $.get("https://amanshah2711.github.io/cs61a/csm/fa19/csm-fa19.html", '', function (data) { $("#main").append(data); });
+    $.get("https://amanshah2711.github.io/cs61a/discussion/su19.html", '', function (data) { $("#main").append(data); });
+    $.get("https://amanshah2711.github.io/cs61a/discussion/sp20.html", '', function (data) { $("#main").append(data); });
+    $.get("https://amanshah2711.github.io/cs61a/csm/sp20/csm-sp20.html", '', function (data) { $("#main").append(data); });
+
+    $("#sp20").click(function() {
+        test("#sp20-content");
+    })
+    $("#csm-sp20").click(function() {
+        test("#csm-sp20-content");
+    })
+    $("#fa19").click(function(){
+        test("#fa19-content");
+    })
+    $("#csm-fa19").click(function() {
+        test("#csm-fa19-content");
+    })
+    $("#su19").click(function(){
+        test("#su19-content");
+    })
+});
