af8b598761609ccaf0b5d43f55c005b5135d5646
Aman <amanshah2711@berkeley.edu>
2025-01-25 09:52:22 -0500

    removing npm related files project uses yarn

--- src/home.js
+++ src/home.js
@@ -2,7 +2,6 @@
 export default function Home() {
     return (
         <div className="container justify-content-center margin-md p-5">
-            <h3>My name is Gabriel</h3>
         </div>
     )
 }
--- src/projects.js
+++ src/projects.js
@@ -1,16 +1,19 @@
 
+import {BsGithub} from "react-icons/bs";
+import { MdOutlineOpenInNew } from "react-icons/md";
 export default function Projects () {
     return (
-        <div className="container justify-content-md-center">
+        <div class="container justify-content-md-center">
             <div class="row row-cols-2 row-cols-md-4 g-4 justify-content-md-center">
             <div class="col">
                 <div class="card">
                 <div class="card-body">
                     <h5 class="card-title">Chess</h5>
-                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    <p class="card-text">Chess was implemented using Python for the backend logic and Flask to create a webserver connecting to the GUI.</p>
                 </div>
                 <div class="card-footer">
-                    <small class="text-muted">Last updated 3 mins ago</small>
+                <a href="https://chess.amanshah2711.me" class="float-end"><MdOutlineOpenInNew /></a>
+                <a href="https://github.com/amanshah2711/chess-react" class="float-end"> <BsGithub/> </a>
                 </div>
                 </div>
             </div>
@@ -21,7 +24,6 @@ export default function Projects () {
                     <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
                 </div>
                 <div class="card-footer">
-                    <small class="text-muted">Last updated 3 mins ago</small>
                 </div>
                 </div>
             </div>
